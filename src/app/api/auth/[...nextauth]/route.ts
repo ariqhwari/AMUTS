1a617f082348b4f189b725bf184241f24265678f
Ariqhwari <ariqhawari02@gmail.com>
2023-12-11 04:11:58 +0700

    fix error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,9 @@ import { Account, User as AuthUser } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "../../../../models/User";
-import connect from "../../../../utils/db"
+import connect from "../../../../utils/db";
 
-export const authOptions: any = {
+const authOptions: any = {
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
