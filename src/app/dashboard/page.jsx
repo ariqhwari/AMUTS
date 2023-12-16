a17d2e3fceecc7ffa9c27c7e26020077c9c6652c
kh-badri <khbadri12@gmail.com>
2023-12-16 20:35:35 +0700

    fix-responsive

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -21,20 +21,20 @@ const Dashboard = async () => {
             {/* Navbar */}
             <Navbar />
             {/* Container */}
-            <div className="sm:flex flex flex-1">
+            <div className="sm:flex ">
                 {/* Bagian 30% */}
-                <div className="inline w-1/6 md:w-1/12 rounded-xl bg-white md:m-2 m-4 shadow-xl ">
+                <div className="inline rounded-xl bg-white md:m-2 m-0 shadow-xl">
                     {/* Konten untuk pemilihan data */}
                     <h2 className="md:p-4 p-2 md:text-lg text-base text-center font-semibold mt-2 ">Pilihan Data</h2>
                     <ul className="md:p-4 p-2 grid grid-flow-row md:text-lg text-sm mt-8 w-auto justify-center">
-                        <Link href={'/dashboard'} className="md:m-2 mb-2 text-center shadow-lg bg-orange rounded-xl py-2 px-4">Data</Link>
-                        <Link href={'/dashboard/userlist'} className="md:m-2 mb-2 text-center border rounded-xl py-2 px-4">Account</Link>
+                        <Link href={'/dashboard'} className="m-2 text-center font-medium border rounded-2xl bg-orange py-2 px-4">Data</Link>
+                        <Link href={'/dashboard/userlist'} className="m-2 text-center font-medium shadow-lg rounded-2xl py-2 px-4">Account</Link>
                     </ul>
                 </div>
                 {/* Bagian 70% */}
-                <div className=" bg-white rounded-xl md:p-2 p-5 md:m-2 m-4 shadow-xl w-screen md:w-full">
+                <div className=" bg-white rounded-xl p-5 m-4 w-full shadow-xl">
                     {/* Tampilan Data */}
-                    <h2 className=" md:py-5 py-10 text-center text-xl font-semibold">Tampilan Data</h2>
+                    <h2 className=" text-center py-10 text-xl font-semibold">Tampilan Data</h2>
                     <Datalist />
                 </div>
             </div>
--- src/components/list/datalist.jsx
+++ src/components/list/datalist.jsx
@@ -1,73 +1,92 @@
-import React from 'react'
+import React from "react";
 import RemoveData from "../button/removeData";
 import { HiPencilAlt } from "react-icons/hi";
-import Link from 'next/link';
+import Link from "next/link";
 
 const getdata = async () => {
-    try {
-        const res = await fetch("https://amuts.vercel.app/api/data", {
-            cache: "no-store",
-        });
+  try {
+    const res = await fetch("https://amuts.vercel.app/api/data", {
+      cache: "no-store",
+    });
 
-        if (!res.ok) {
-            throw new Error("Failed to fetch data");
-        }
-
-        return res.json();
-    } catch (error) {
-        console.log("Error loading data: ", error);
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
     }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading data: ", error);
+  }
 };
 
 const Datalist = async () => {
+  try {
+    const { data } = await getdata();
+    return (
+      <table className="w-auto border-collapse border">
+        <thead>
+          <tr className="bg-hijau3">
+            <th className=" border border-gray-400 md:text-sm text-xs  md:py-2 md:p-5">
+              Nama
+            </th>
+            <th className=" border border-gray-400 md:text-sm text-xs  md:py-2 md:p-5">
+              Alamat
+            </th>
+            <th className=" border border-gray-400 md:text-sm text-xs  md:py-2 md:p-5">
+              Telepon
+            </th>
+            <th className=" border border-gray-400 md:text-sm text-xs  md:py-2 md:p-5">
+              Kg
+            </th>
+            <th className=" border border-gray-400 md:text-sm text-xs  md:py-2 md:p-5">
+              Sampah
+            </th>
+            <th className=" border border-gray-400 md:text-sm text-xs  md:py-2 md:p-5">
+              Harga (Rp.)
+            </th>
+            <th className=" border border-gray-400 md:text-sm text-xs  md:py-2 md:p-5">
+              Act
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d) => (
+            <tr key={d._id} className="text-center">
+              <td className="bg-slate-50 border border-gray-400 md:text-sm text-xs  md:py-2 py-0">
+                {d.nama}
+              </td>
+              <td className="bg-slate-50 border border-gray-400 md:text-sm text-xs  md:py-2 py-0">
+                {d.alamat}
+              </td>
+              <td className="bg-slate-50 border border-gray-400 md:text-sm text-xs  md:py-2 py-0 px-1">
+                {d.noTelp}
+              </td>
+              <td className="bg-slate-50 border border-gray-400 md:text-sm text-xs  md:py-2 py-0">
+                {d.jumlahSampah}
+              </td>
+              <td className="bg-slate-50 border border-gray-400 md:text-sm text-xs  md:py-2 py-0">
+                {d.jenisSampah}
+              </td>
+              <td className="bg-slate-50 border border-gray-400 md:text-sm text-xs  md:py-2 py-0">
+                {d.totalHarga}
+              </td>
+              <td className="bg-slate-50 border border-gray-400 md:text-sm text-xs md:py-2 py-0 md:px-0">
+                <div className="flex items-center justify-center">
+                  <Link href={`dashboard/edit/${d.id}`} className="">
+                    <HiPencilAlt size={24} />
+                  </Link>
+                  <RemoveData id={d._id} />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <div>Error fetching data</div>;
+  }
+};
 
-    try {
-        const { data } = await getdata();
-        return (
-
-            <table className="w-full md:w-auto border-collapse border">
-                <thead>
-                    <tr className='bg-hijau3'>
-                        <th className=" border border-gray-400 md:text-sm text-xs md:py-2 md:p-5">Nama</th>
-                        <th className=" border border-gray-400 md:text-sm text-xs md:py-2 md:p-5">Alamat</th>
-                        <th className=" border border-gray-400 md:text-sm text-xs md:py-2 md:p-5">Telepon</th>
-                        <th className=" border border-gray-400 md:text-sm text-xs md:py-2 md:p-5">Kg</th>
-                        <th className=" border border-gray-400 md:text-sm text-xs md:py-2 md:p-5">Sampah</th>
-                        <th className=" border border-gray-400 md:text-sm text-xs md:py-2 md:p-5">Harga (Rp.)</th>
-                        <th className=" border border-gray-400 md:text-sm text-xs md:py-2 md:p-5">Act</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((d) => (
-                        <tr key={d._id} className='text-center'>
-                            <td className="border border-gray-400 md:text-sm text-xs md:py-2 py-2 px-4 md:px-0">{d.nama}</td>
-                            <td className="border border-gray-400 md:text-sm text-xs md:py-2 py-2 px-4 md:px-0">{d.alamat}</td>
-                            <td className="border border-gray-400 md:text-sm text-xs md:py-2 py-2 px-4 md:px-0">{d.noTelp}</td>
-                            <td className="border border-gray-400 md:text-sm text-xs md:py-2 py-2 px-4 md:px-0">{d.jumlahSampah}</td>
-                            <td className="border border-gray-400 md:text-sm text-xs md:py-2 py-2 px-4 md:px-0">{d.jenisSampah}</td>
-                            <td className="border border-gray-400 md:text-sm text-xs md:py-2 py-2 px-4 md:px-0">{d.totalHarga}</td>
-                            <td className="border border-gray-400 md:text-sm text-xs md:py-2 py-2 px-4 md:px-0">
-                                <div className='flex items-center justify-center'>
-                                    <Link href={`dashboard/edit/${d.id}`} className=''>
-                                        <HiPencilAlt size={24} />
-                                    </Link>
-
-                                    <RemoveData id={d._id} />
-
-                                </div>
-                            </td>
-
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        )
-
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return <div>Error fetching data</div>;
-    }
-
-}
-
-export default Datalist
+export default Datalist;
